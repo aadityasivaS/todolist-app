691db9c3b1fcbbc572cdd93098b236c71278d014
Aadityasiva <67770098+aadityasivaS@users.noreply.github.com>
2021-04-11 11:33:21 +0530

    added bottom-nav-bar

--- lib/screens/app/app.dart
+++ lib/screens/app/app.dart
@@ -8,6 +8,7 @@ class AppScreen extends StatefulWidget {
 
 class _AppScreenState extends State<AppScreen> {
   FirebaseAuth auth = FirebaseAuth.instance;
+  int currentPage = 0;
   String greeting() {
     var hour = DateTime.now().hour;
     if (hour < 12) {
@@ -23,6 +24,41 @@ class _AppScreenState extends State<AppScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  greeting(),
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentPage,
+        onTap: (index) => setState(() => currentPage = index),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Your lists',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Starred',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
     );
   }
 }
