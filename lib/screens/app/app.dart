e4002bc49a5a000a6d1b1a4a4455835a15b89e7a
Aadityasiva <67770098+aadityasivaS@users.noreply.github.com>
2021-04-12 10:12:46 +0530

    In app.dart removed the greeting to move into the your lists tab

--- lib/screens/app/app.dart
+++ lib/screens/app/app.dart
@@ -9,38 +9,11 @@ class AppScreen extends StatefulWidget {
 class _AppScreenState extends State<AppScreen> {
   FirebaseAuth auth = FirebaseAuth.instance;
   int currentPage = 0;
-  String greeting() {
-    var hour = DateTime.now().hour;
-    if (hour < 12) {
-      return 'Good Morning';
-    }
-    if (hour < 17) {
-      return 'Good Afternoon';
-    }
-    return 'Good Evening';
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: Text(
-                  greeting(),
-                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentPage,
         onTap: (index) => setState(() => currentPage = index),
