da350e03b466b613116ccd76bdba44f4fb6ae35c
Aadityasiva <67770098+aadityasivaS@users.noreply.github.com>
2021-04-15 13:54:20 +0530

    added makelist functionality

--- lib/screens/app/app.dart
+++ lib/screens/app/app.dart
@@ -1,10 +1,16 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:todolist/components/errorAlert.dart';
 import 'package:todolist/components/input.dart';
 import 'package:todolist/screens/app/tabs/lists.dart';
 import 'package:todolist/screens/app/tabs/settings.dart';
 import 'package:todolist/screens/app/tabs/starred.dart';
+import 'package:http/http.dart' as http;
 
 class AppScreen extends StatefulWidget {
   @override
@@ -24,6 +30,22 @@ class _AppScreenState extends State<AppScreen> {
     super.initState();
   }
 
+  Future<String?> getImage(title) async {
+    var request = http.Request(
+      'GET',
+      Uri.parse(
+        'https://pixabay.com/api/?key=${env['PIXABAY_API_KEY']}&q=$title&per_page=3',
+      ),
+    );
+    http.StreamedResponse response = await request.send();
+    if (response.statusCode == 200) {
+      var res = await response.stream.bytesToString();
+      return jsonDecode(res)['hits'][0]['webformatURL'];
+    } else {
+      showErrorDialog(context, 'Error', response.reasonPhrase);
+    }
+  }
+
   void addUserDocIfNotExists(uid) async {
     await db.doc('users/$uid').get().then((doc) {
       if (!doc.exists) {
@@ -34,6 +56,7 @@ class _AppScreenState extends State<AppScreen> {
 
   @override
   Widget build(BuildContext context) {
+    String uid = auth.currentUser!.uid;
     return Scaffold(
       backgroundColor: Colors.white,
       body: tabs[currentPage],
@@ -72,7 +95,8 @@ class _AppScreenState extends State<AppScreen> {
                 return SafeArea(
                   child: SingleChildScrollView(
                     child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 60, horizontal: 30),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 60, horizontal: 30),
                       child: Column(
                         children: [
                           Text(
@@ -88,7 +112,29 @@ class _AppScreenState extends State<AppScreen> {
                             center: true,
                           ),
                           ElevatedButton.icon(
-                            onPressed: () {},
+                            onPressed: () async {
+                              if (modalNewList.text != '') {
+                                EasyLoading.show(status: 'Please wait...');
+                                getImage(modalNewList.text).then((value) {
+                                  db.doc('users/$uid').collection('lists').add({
+                                    'title': modalNewList.text,
+                                    'imageURL': value.toString()
+                                  }).then((value) {
+                                    setState(() {
+                                      modalNewList.text = '';
+                                      EasyLoading.dismiss();
+                                    });
+                                  });
+                                });
+                              } else {
+                                EasyLoading.dismiss();
+                                showErrorDialog(
+                                  context,
+                                  'Error',
+                                  'Please provide a list title',
+                                );
+                              }
+                            },
                             icon: Icon(Icons.add),
                             label: Text('Add Task'),
                           ),
